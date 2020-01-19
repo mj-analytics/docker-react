675982fffbcc297c2389e23d6df2579949bb2114
Jahn <markus.jahn@capgemini.com>
2020-01-19 15:37:41 +0100

    Changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>src/App.js</code> and save to reload.
+           This is amazing.
         </p>
         <a
           className="App-link"
